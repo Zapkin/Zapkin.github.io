6a2109afe46429819ccea116f988d87df5126ac9
zapkin <zapkinwen@gmail.com>
2024-11-30 20:04:03 +0800

    refactor: 初见雏形

--- src/components/post/PostCopyright.tsx
+++ src/components/post/PostCopyright.tsx
@@ -1,6 +1,5 @@
 import { author, site } from '@/config.json'
 import { getFormattedDateTime } from '@/utils/date'
-import { AnimatedSignature } from '../AnimatedSignature'
 import { useEffect, useState } from 'react'
 import { toast } from "react-toastify";
 
@@ -43,7 +42,6 @@ export function PostCopyright({
       <hr className="my-3 border-primary" />
       <div>
         <div className="float-right ml-4 my-2">
-          <AnimatedSignature />
         </div>
         <p>
           商业转载请联系站长获得授权，非商业转载请注明本文出处及文章链接，您可以自由地在任何媒体以任何形式复制和分发作品，也可以修改和创作，但是分发衍生作品时必须采用相同的许可协议。
