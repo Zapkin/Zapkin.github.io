da1e878820e14dc8163fa2fe0a6f9aa5caf74fb0
Dumbass Design <36330030+Zapkin@users.noreply.github.com>
2024-12-03 18:57:42 +0800

    refactor: new articles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,9 +20,9 @@ const config: Config = {
     fontSize: {
       xs: '0.75rem',
       sm: '0.875rem',
-      base: '1rem',
-      lg: '1.125rem',
-      xl: '1.25rem',
+      base: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.375rem',
       '2xl': '1.5rem',
       '3xl': '1.875rem',
       '4xl': '2.25rem',
@@ -53,6 +53,9 @@ const config: Config = {
       zIndex: {
         '1': '1',
       },
+    },
+    screens: {
+      lg: '960px',
     },
   },
 }
